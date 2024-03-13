fb64882742868793d34ff5b15229cf37bbb54137
isaac-tech-01 <isaac.ayeni@optimusbank.com>
2024-03-13 15:15:26 +0100

    email fix

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form action="https://getform.io/f/zazokymb" method="POST"  className={`flex flex-col max-w-[600px] w-full`}>
             <div className={`pb-8`}>
                 <p className={`text-4xl font-bold inline border-b-4 border-pink-600`}>Contact</p>
-                <p className={`py-4`}>// Submit the form below or reach out to me on <a href="ayeniisaac@gmail.com">ayeniisaac1on1@gmail.com</a></p>
+                <p className={`py-4`}>// Submit the form below or reach out to me on <a href="mailto:ayeniisaac1on1@gmail.com">ayeniisaac1on1@gmail.com</a></p>
             </div>
             <input className={`p-2 bg-[#ccd6f6] text-black`} type="text" placeholder='Name' name='name' />
             <input className={`my-4 p-2 bg-[#ccd6f6] text-black`} type="email" placeholder='Email' name='email' />
